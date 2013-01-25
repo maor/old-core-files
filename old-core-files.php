a7e5f91c56042eba2edfc563cd3b6962fe9a3e0d
Maor Chasen <maor@illuminea.com>
2013-01-25 23:42:34 +0200

    l10n, action links, more

--- old-core-files.php
+++ old-core-files.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: Old Core Files
- Plugin URI: http://www.wp-tricks.co.il/cool-slug-goes-here
+ Plugin URI: http://www.wp-tricks.co.il/old_core_files
  Description: Old Core Files notifies the user when old core files which are due removal exist in the filesystem
  Author: Maor Chasen, Rami Yushuvaev
  Author URI: http://maorchasen.com
@@ -12,7 +12,7 @@
 /**
  * Description:
  * 
- * When core is being upgraded, usually some files are no longer used by WP, and they are set for removal.
+ * When core is being upgraded, usually some files are no longer used by WordPress, and they are set for removal.
  * On some occasions, PHP has no permissions to delete these files, and they stay on the server, possibly
  * exposing your site to attackers.
  *
@@ -28,6 +28,8 @@
  * @since 1.0
  */
 class Old_Core_Files {
+
+	private static $basename;
 
 	private $page;
 	private $parent_slug = 'tools.php';
@@ -52,14 +54,36 @@ class Old_Core_Files {
 	function action_init() {
 		if ( ! is_admin() )
 			return;
-		
+
+		isset( self::$basename ) || self::$basename = plugin_basename( __FILE__ );
+
+		// Add OCF action links
+		add_filter( 'plugin_action_links_' . self::$basename, array( $this, 'action_links' ), 10, 2 );
+
+		// Add OCF admin menu
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+
+		// Add OCF meta boxes
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
 
 		// Allow the view to be placed elsewhere than tools.php
 		$this->parent_slug = apply_filters( 'ocf_parent_slug', $this->parent_slug );
+
 		// Hijack the default capability for viewing the page
 		$this->view_cap = apply_filters( 'ocf_view_cap', $this->view_cap );
+
+		// Load our textdomain to allow multilingual translations
+		load_plugin_textdomain( 'ocf', false, self::$basename . '/languages/' );
+	}
+
+	/**
+	 * Register action links for OCF
+	 *
+	 * @since 1.0
+	 */
+	function action_links( $links, $file ) {
+		$links[] = sprintf( '<a href="%s">%s</a>', menu_page_url( $this->page_slug, false ), __( 'Settings' /*, 'ocf'*/ ) );
+		return $links;
 	}
 
 	/**
@@ -68,7 +92,8 @@ class Old_Core_Files {
 	 * @since 1.0
 	 */
 	function admin_menu() {
-		$this->page = add_submenu_page( $this->parent_slug, __( 'Old Core Files', 'oc-files' ), __( 'Old Core Files', 'core-control' ), $this->view_cap, $this->page_slug, array( $this, 'dashboard_page' ) );
+		$this->page = add_submenu_page(
+			$this->parent_slug, __( 'Old Core Files', 'ocf' ), __( 'Old Core Files', 'ocf' ), $this->view_cap, $this->page_slug, array( $this, 'dashboard_page' ) );
 
 		// Add callbacks for this screen only 
 		add_action( "load-$this->page", array( $this, 'page_actions' ), 9 );
@@ -112,8 +137,8 @@ class Old_Core_Files {
 	 * @since 1.0
 	 */
 	function add_meta_boxes() {
-		add_meta_box( 'list-files', __( 'Old Core Files', 'oc-files' ), array( $this, 'metabox_list_files' ), $this->page, 'normal', 'high' );
-		add_meta_box( 'about', __( 'About', 'oc-files' ), array( $this, 'metabox_about' ), $this->page, 'side', 'high' );
+		add_meta_box( 'list-files', __( 'Old Core Files', 'ocf' ), array( $this, 'metabox_list_files' ), $this->page, 'normal', 'high' );
+		add_meta_box( 'about', __( 'About', 'ocf' ), array( $this, 'metabox_about' ), $this->page, 'side', 'high' );
 	}
 
 	/**
@@ -127,17 +152,24 @@ class Old_Core_Files {
 		// Require the file that stores $_old_files
 		require_once ABSPATH . 'wp-admin/includes/update-core.php';
 
+		/**
+		 * Before we initialize WP_Filesystem, we should make sure the right
+		 * filesystem transport is set
+		 */
+		add_filter( 'filesystem_method', array( $this, 'assert_filesystem_method' ), 10, 2 );
+
 		// If $wp_filesystem isn't there, make it be there!
 		if ( ! $wp_filesystem )
 			WP_Filesystem();
 
-		// Not sure why I had to add this. Maybe shuffling through the filesystem
-		// can take up more time than usual. Thought maybe we should cache the results
-		// but there is really no point in doing so, since data must be real-time.
+		/**
+		 * Not sure why I had to add this. Maybe shuffling through the filesystem
+		 * can take up more time than usual. Thought maybe we should cache the results
+		 * but there is really no point in doing so, since data must be real-time.
+		 */
 		@set_time_limit( 300 );
 
 		$path_to_wp = trailingslashit( $wp_filesystem->abspath() );
-
 		$existing_old_files = array();
 
 		// Pile up old, existing files
@@ -145,35 +177,63 @@ class Old_Core_Files {
 			if ( $wp_filesystem->exists( $path_to_wp . $old_file ) )
 				$existing_old_files[] = $old_file;
 		}
+		?>
+		<ul class="subsubsub">
+			<li class="all"><a href="<?php echo esc_url( add_query_arg( 'show', 'all' ) ); ?>"><?php echo __( 'All', 'ocf' ); ?> <span class="count">(<?php echo count( $_old_files ); ?>)</span></a> |</li>
+			<li class="existing"><a href="<?php echo esc_url( add_query_arg( 'show', 'existing' ) ); ?>" class="current"><?php echo __( 'Existing', 'ocf' ); ?> <span class="count">(<?php echo count( $existing_old_files ); ?>)</span></a></li>
+		</ul>
+		
+		<br class="clear" />
 
+		<?php
 		if ( ! empty( $existing_old_files ) ) :
 			?>
-			<p><?php esc_html_e( 'We have found some old files in this WordPress installation. Please review the files below.', 'oc-files' ); ?></p>
-			<table class="widefat post-revisions" cellspacing="0">
+			<p><?php esc_html_e( 'We have found some old files in this WordPress installation. Please review the files below.', 'ocf' ); ?></p>
+
+			<table class="widefat" cellspacing="0">
 				<thead>
 					<tr>
-						<th scope="col"><?php esc_html_e( 'File', 'oc-files' ); ?></th>
-						<th scope="col" class="action-links"><?php esc_html_e( 'Actions', 'oc-files' ); ?></th>
+						<th scope="col"><?php esc_html_e( 'File', 'ocf' ); ?></th>
+						<th scope="col" class="action-links"><?php esc_html_e( 'Actions', 'ocf' ); ?></th>
 					</tr>
 				</thead>
 				<tbody>
-			<?php foreach ( $existing_old_files as $existing_file ) : ?>
+				<?php foreach ( $existing_old_files as $existing_file ) : ?>
 					<tr>
-						<td><code><?php echo esc_html( $existing_file ); ?></code></td>
+						<td>
+							<code><?php echo esc_html( $existing_file ); ?></code>
+						</td>
+						<td class="action-links">
+							<?php if ( current_user_can( $this->view_cap ) ) : // Double check befor allowing 'delete' action ?>
+							<span class="trash"><a href="<? admin_url( $this->parent_slug . '?page=' . $this->page_slug ); /* Add nonce, Add 'action=delete', Add File name (for deletion) */ ?>"><?php echo __( 'Delete', 'ocf' ); ?></a></span>
+							<?php endif; ?>
+						</td>
 					</tr>
-			<?php endforeach; ?>
+				<?php endforeach; ?>
 				</tbody>
+				<tfoot>
+					<tr>
+						<td>
+							<strong><?php printf( __( 'Old Files Count: %d', 'ocf' ), count( $existing_old_files ) ); ?></strong>
+						</td>
+						<td class="action-links">
+							<?php if ( current_user_can( $this->view_cap ) ) : // Double check befor allowing 'delete' action ?>
+							<span class="trash"><a href="<?php echo admin_url( add_query_arg( 'action', 'trash-all' ) ); /* Add nonce, Add 'action=delete', Add File name (for deletion) */ ?>"><?php echo __( 'Delete All', 'ocf' ); ?></a></span>
+							<?php endif; ?>
+						</td>
+					</tr>
+				</tfoot>
 			</table><?php
 		else: ?>
-			<p><?php esc_html_e( 'Seems like there are no old files in your installation. Hooray!', 'oc-files' ); ?></p>
+			<p><?php esc_html_e( 'Seems like there are no old files in your installation. Dont forget to delete old WordPress files after each upgrade.', 'ocf' ); ?></p>
 			<?php
 		endif;
 	}
 
 	function metabox_about() {
 		?>
-		<h4><?php esc_html_e( 'What is this about?', 'oc-files' ); ?></h4>
-		<p><?php esc_html_e( 'When core is being upgraded, usually some files are no longer used by WP, and they are set for removal. On some occasions, PHP has no permissions to delete these files, and they stay on the server, possibly exposing your site to attackers.', 'oc-files' ); ?></p>
+		<h4><?php esc_html_e( 'What is this about?', 'ocf' ); ?></h4>
+		<p><?php esc_html_e( 'When core is being upgraded, usually some files are no longer used by WordPress, and they are set for removal. On some occasions, PHP has no permissions to delete these files, and they stay on the server, possibly exposing your site to attackers.', 'ocf' ); ?></p>
 		<?php
 	}
 
@@ -187,8 +247,16 @@ class Old_Core_Files {
 		<div class="wrap">
 
 			<?php screen_icon(); ?>
-			<h2><?php echo esc_html__( 'Old Core Files', 'oc-files' ); ?></h2>
-			
+			<h2><?php echo esc_html__( 'Old Core Files', 'ocf' ); ?></h2>
+
+			<?php // We can add a FAQ tab with the full list of files.
+			/*
+			<h2 class="nav-tab-wrapper">
+				<a class="nav-tab nav-tab-active" href="<?php echo admin_url( $this->parent_slug.'?page='.$this->page_slug ); ?>"><?php echo __( 'Action', 'ocf' ); ?></a>
+				<a class="nav-tab" href="<?php echo admin_url( $this->parent_slug.'?page='.$this->page_slug ); ?>"><?php echo __( 'FAQ', 'ocf' ); ?></a>
+			</h2>*/
+			?>
+
 			<form name="oldfiles" method="post">
 				<input type="hidden" name="action" value="some-action">
 				<?php wp_nonce_field( 'some-action-nonce' );
@@ -198,7 +266,7 @@ class Old_Core_Files {
 				wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ); ?>
 
 				<div id="poststuff">
-		
+
 					<div id="post-body" class="metabox-holder columns-<?php echo 1 == get_current_screen()->get_columns() ? '1' : '2'; ?>"> 
 
 						<!-- We had the description of the plugin here, moved to the metabox.
@@ -223,9 +291,19 @@ class Old_Core_Files {
 	<?php
 	}
 
+	function assert_filesystem_method( $method, $args ) {
+		/**
+		 * Here is where we need to do a crazy calculation of
+		 * what type of filesystem transport we should use
+		 * to ensure the files get removed properly.
+		 */
+		return $method;
+	}
+
 	function activate() {
 		// Nothing to do here right now
 	}
 }
+
 global $old_core_files_instance;
 $old_core_files_instance = new Old_Core_Files;
